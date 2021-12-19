7736dbe0cf72c44f804e9851c10bcfc72e329f05
eriq <e.abonie@gmail.com>
2021-12-19 19:43:39 +0000

    app comms with db

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@
 const express = require('express');
 const app = express();
 
+const artistRouter = require('./routes/artist');
+
 app.use(express.json());
+
+app.use('/artist', artistRouter);
 
 
 app.get('/', (req,res)=> {
     res.status(200).json({result:'Hello world Eriq'});
 });
+
+
 
 
 module.exports = app;
