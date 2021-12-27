086859eb458a374489d5b099ae85fd22503b8cf2
eriq <e.abonie@gmail.com>
2021-12-27 16:34:51 +0000

    added swagger

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@
 const express = require('express');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const artistRouter = require('./routes/artist');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 
